da9eca2fac144bb688962e524b589f5eb8c54d3b
HaiSieuNhan <daohai2000.hl@gmail.com>
2020-11-12 11:42:52 +0700

    Add project files.

---
+++ Bai_1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bai_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bai 1:
+            //Viết console application để thực hiện hoá hàm sau, và in ra kết quả lên màn hình console
+
+            //public string[][] GhepPhanTuNangCao(params string[][] args)
+            //{
+            //    // đầu vào là 1 mảng, trong mỗi phần tử là 1 mảng 1 chiều
+            //    //vd: args=[ ["1","2"], ["a","b"], ["x","y","z"] ];
+            //    //return [[ "1", "a","x" ], [ "1", "a","y" ] , [ "1", "a","z" ] , ... , , [ "2", "b","z" ]]
+            //}
+
+            List<List<string>> Foo = new List<List<string>>() { };
+            List<string> list = new List<string>() { "1", "2" };
+            List<string> list1 = new List<string>() { "a", "b" };
+            List<string> list2 = new List<string>() { "x", "y", "z" };
+            Foo.Add(list);
+            Foo.Add(list1);
+            Foo.Add(list2);
+            GhepPhanTuNangCao(Foo);
+        }
+        static void GhepPhanTuNangCao(List<List<string>> Foo)
+        {
+            List<List<string>> result = new List<List<string>>();
+            for (int i = 0; i < Foo.Count - 1;)
+            {
+                for (int x = 0; x < Foo[i].Count; x++)
+                {
+                    List<String> temp2 = Foo.ElementAt(i + 1);
+                    for (int j = 0; j < temp2.Count; j++)
+                    {
+                        List<String> temp3 = Foo.ElementAt(i + 2);
+                        for (int k = 0; k < temp3.Count; k++)
+                        {
+                            List<string> arrString = new List<string>();
+                            arrString.Add(Foo[i][x]);
+                            arrString.Add(temp2[j]);
+                            arrString.Add(temp3[k]);
+                            result.Add(arrString);
+                        }
+                    }
+                }
+                break;
+            }
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.Write("Mang Thu "+i+" :");
+                for (int j = 0; j < result[i].Count; j++)
+                {
+                    Console.Write("\t"+result[i][j]);
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+
+            }
+
+        }
+
+    }
+}
---
+++ Bai_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Bai_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Viết console application để thực hiện hoá hàm sau, và in ra kết quả lên màn hình console
+
+            //public string ChangeToText(long NumberOfVersion)
+            //{
+            //    // do your code here
+            //    // if NumberOfVersion < 1000000 return text (eg: NumberOfVersion =1 return "Mot", NumberOfVersion=99999 return "Chin muoi chin nghin chin tram chin chin" ...
+            //    //else return NumberOfVersion.ToString()
+            //}
+            Console.WriteLine("Nhap vao So Cua Ban ; ");
+            string[] Ones = { "Một", "Hai", "Ba", "Bốn", "Năm", "Sáu", "Bảy", "Tám", "Chín", "Mười", "Mười một", "Mười hai", "Mười ba", "Mười bốn", "Mười lăm", "Mười sáu", "Mười bảy", "Mười tám", "Mười chín" };
+            string[] Tens = { "Mười", "Hai Mươi", "Ba Mươi", "Bốn Mươi", "Năm Mươi", "Sáu Mươi", "Bảy Mươi", "Tám Mươi ", "Chín Mươi " };
+            int number = int.Parse(Console.ReadLine());
+            string strWords = "";
+            if (number > 99999 && number < 1000000)
+            {
+                int i = number / 100000;
+                strWords = strWords + Ones[i - 1] + " Trăm ";
+                number = number % 100000;
+            }
+            if (number > 9999 && number < 100000)
+            {
+                int i = number / 10000;
+                strWords = strWords + Ones[i - 1] + " Mươi ";
+                number = number % 10000;
+            }
+            if (number > 999 && number < 10000)
+            {
+                int i = number / 1000;
+                strWords = strWords + Ones[i - 1] + "  Nghìn ";
+                number = number % 1000;
+            }
+            if (number > 99 && number < 1000)
+            {
+                int i = number / 100;
+                strWords = strWords + Ones[i - 1] + " Trăm ";
+                number = number % 100;
+            }
+            if (number > 19 && number < 100)
+            {
+                int i = number / 10;
+                strWords = strWords + Tens[i - 1] + " ";
+                number = number % 10;
+            }
+            if (number > 0 && number < 20)
+            {
+                strWords = strWords + Ones[number - 1];
+            }
+            Console.WriteLine(strWords);
+        }
+    }
+}
